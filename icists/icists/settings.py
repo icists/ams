9fa235a91d3927b3ceb8a02d20f9ada51f6a8ed9
Shavakan <chiyah92@gmail.com>
2015-04-27 20:50:06 +0900

    startapp registration, dependent settings done

--- icists/icists/settings.py
+++ icists/icists/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'icists.apps.registration',
 )
 
 MIDDLEWARE_CLASSES = (
--- icists/icists/urls.py
+++ icists/icists/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.http import HttpResponse
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'icists.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', lambda request: HttpResponse('Hello, World!')),
+    url(r'^registration/', include('icists.apps.registration.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
