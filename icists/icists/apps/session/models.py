5d6f8311335885855d14e7565d8f81992763eb10
Sam Brad Jo <a1sams1a@gmail.com>
2015-04-29 21:02:49 +0900

    add login, logout functions (refs #4, #6)

--- icists/icists/apps/session/models.py
+++ icists/icists/apps/session/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    birthday = models.DateField()
+    nationality = models.CharField(max_length = 45)
+    gender = models.CharField(max_length = 45)
+    phone = models.CharField(max_length = 45) #country_code + number
+    major = models.CharField(max_length = 45)
+    university = models.CharField(max_length = 70)
+    picture = models.ImageField(upload_to = 'profilepicture') #size limit to 2000*2000
+    how_you_found_us = models.CharField(max_length = 100)
--- icists/icists/settings.py
+++ icists/icists/settings.py
@@ -14,7 +14,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'icists.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ os.path.join(PROJECT_DIR, 'icists/templates') ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -109,9 +109,3 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
-
-# Template files (HTML)
-
-TEMPLATE_DIRS = (
-    os.path.join(PROJECT_DIR, 'templates'),
-)
