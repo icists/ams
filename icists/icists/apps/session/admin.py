7619848355cd7fde3ddb70974a96cfd24243e781
Sam Brad Jo <a1sams1a@gmail.com>
2015-05-30 18:02:07 +0900

    Add applied/not applied filter to user list view

--- icists/icists/apps/session/admin.py
+++ icists/icists/apps/session/admin.py
@@ -21,6 +21,24 @@ class UserResource(resources.ModelResource):
     class Meta:
         model = User
 
+class StatusFilter(admin.SimpleListFilter):
+    title = 'application status'
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('yes', 'Applied'),
+            ('no', 'Not Applied'),
+        )
+
+    def queryset(self, request, qs):
+        if self.value() == 'yes':
+            return qs.filter(application__isnull=False)
+        
+        if self.value() == 'no':
+            return qs.filter(application__isnull=True)
+
+
 class UserAdmin(ImportExportModelAdmin):
     def get_name(self, obj):
         return obj.first_name + ' ' + obj.last_name
@@ -44,7 +62,7 @@ class UserAdmin(ImportExportModelAdmin):
 
     list_display = ('get_name', 'get_university', 'get_major', 'email', 'get_phone', 'is_staff')
     inlines = (UserProfileInline, )
-
+    list_filter = (StatusFilter, 'userprofile__university')
     resource_class = UserResource
     
     #list_filter = ('project_topic', 'visa_letter_required', 'financial_aid', 'previously_participated', 'screening_result', 'application_category', 'user__userprofile__university')
