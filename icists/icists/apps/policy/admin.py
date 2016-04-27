577ff7f41ca33890eaf452dffc0d665fa55ccfd6
Park Junwoo <junwoopark0130@gmail.com>
2016-04-27 17:25:16 +0900

    configuration model limited to one row.

--- icists/icists/apps/policy/admin.py
+++ icists/icists/apps/policy/admin.py
@@ -4,10 +4,18 @@ from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
 
-
 # Register your models here.
 class ConfigurationAdmin(ImportExportModelAdmin):
     list_display = ('application_stage', 'year')
+
+    def has_add_permission(self, request):
+        # if there's already an entry, do not allow adding
+        count = Configuration.objects.all().count()
+        if count == 0:
+            return True
+        else:
+            return False
+
 
 class PriceAdmin(ImportExportModelAdmin):
     pass
