8c4b4488935251ecf79bdf91dc9765f3481f0d1d
Park Junwoo <junwoopark0130@gmail.com>
2016-11-10 01:45:19 +0000

    additional instructions for installation and running.

--- run.sh
+++ run.sh
@@ -1,3 +1,16 @@
 #! /bin/bash
+
+
+# first run
+# get ams_base/icists/mysql.cnf
+# get ams_base/secret.key
+# create database application_icists
+
+# dump application_icists database
+# mysqldump -u [user] -p application_icists > filename.sql
+# mysql -u [user] -p application_icists < filename.sql
+
+
+
 source env/bin/activate
 python ams_base/manage.py runserver --insecure 0.0.0.0:21034
